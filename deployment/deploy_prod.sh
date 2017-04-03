50b8c9ab5d52ab2c2d13d2d2cc146de3651c2f26
Jahongir Rahmonov  =) <jrahmonov2@gmail.com>
2017-04-03 22:44:37 +0500

    Change placeholder to read NNAAAMMMEEE

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -5,6 +5,7 @@ ssh root@104.236.57.112 <<EOF
   git pull
   source /opt/envs/djtrump/bin/activate
   pip install -r requirements.txt
+  ./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
 EOF
